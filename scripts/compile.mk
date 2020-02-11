e01625ff732f1a88886d8b7efea2284806d378bc
suchuanpao <suchuanpao@outlook.com>
2020-02-11 17:51:51 +0800

    make it complete

--- scripts/compile.mk
+++ scripts/compile.mk
@@ -1,33 +1,47 @@
-CXX:=$(XS_CROSS_COMPILE)g++
-CC:=$(XS_CROSS_COMPILE)gcc
+CPP:=$(SCP_CROSS_COMPILE)g++
+CC:=$(SCP_CROSS_COMPILE)gcc
 
-LIBS=-lpthread
-INCLUDES=-I$(XS_INCLUDE)
-CXXFLAGS=-O2 -Wall -g
-LCXXFLAGS=-L$(XS_LIB)
-DCXXFLAGS=-M
+COMPILE:=$(CC)
 
-OBJS=$(patsubst %.cpp, %.o, $(SRCS))
-DEPS=$(patsubst %.cpp, %.d, $(SRCS))
-DEPS_TARGET=$(XS_PREFIX)/deplists
+SRCS += $(C_SRCS) $(CPP_SRCS)
 
-bin:$(OBJS)
-	$(CXX) -o $(TARGET) $(OBJS) $(CXXFLAGS) $(LIBS) $(LCXXFLAGS)
+C FLAGS += -I$(SCP_INCLUDE)
+C_FLAGS += -Os -Wall -g
+C_LIBS += -lpthread -ldl -lm
+C_LDFLAGS += -L$(SCP_LIB)
+C_DFLAGS += -M
+
+CPP_FLAGS = -O2 -Wall -g
+#CPP_LIBS += -lstdc++
+CPP_LIBS += -lstdc++
+CPP_LDFLAGS = -L$(SCP_LIB)
+CPP_DFLAGS = -M
+
+C_OBJS=$(patsubst %.c, %.lo, $(filter %.c, $(SRCS)))
+C_DEPS=$(patsubst %.c, %.d, $(filter %.c, $(SRCS)))
+
+CPP_OBJS=$(patsubst %.cpp, %.oo, $(filter %.cpp, $(SRCS)))
+CPP_DEPS=$(patsubst %.cpp, %.d, $(filter %.cpp, $(SRCS)))
+
+DEPS_TARGET=$(SCP_PREFIX)/.deps
+
+bin:$(C_OBJS) $(CPP_OBJS)
+	$(CC) -o $(TARGET) $(C_OBJS) $(CPP_OBJS) $(CPPFLAGS) $(C_LDFLAGS) $(CPP_LDFLAGS) $(C_LIBS) $(CPP_LIBS)
 
 lib:$(OBJS)
-	$(AR) rc $(TARGET) $(OBJS)
+	$(AR) rc $(TARGET) $(C_OBJS) $(CPP_OBJS)
 
-%.o:%.cpp
-	$(CXX) $(INCLUDES) $(CXXFLAGS) -c $< -o $@
+%.oo:%.cpp
+	$(CPP) $(CPP_FLAGS) -c $< -o $@
 
-%.o:%.c
-	$(CC) $(INCLUDES) $(CFLAGS) -c $< -o $@
+%.lo:%.c
+	$(CC) $(C_FLAGS) -c $< -o $@
 
 %.d:%.cpp
-	@$(CXX) $(INCLUDES) $(DCXXFLAGS) $< >> $(DEPS_TARGET)
+	@$(CPP) $(CPP_DFLAGS) $(CPP_FLAGS) $< >> $(DEPS_TARGET)
 
 %.d:%.c
-	@$(CC) $(INCLUDES) $(DCXXFLAGS) $< >> $(DEPS_TARGET)
+	@$(CC) $(C_DFLAGS) $(C_FLAGS) $< >> $(DEPS_TARGET)
 
 ifneq ($(MAKECMDGOALS), clean)
 -include $(DEPS_TARGET)
@@ -35,4 +49,7 @@ endif
 
 .PHONY : clean
 clean:
-	rm -rf $(DEPS_TARGET) $(TARGET) $(OBJS)
+	rm -rf $(DEPS_TARGET)
+	rm -rf $(TARGET)
+	rm -rf $(CPP_OBJS)
+	rm -rf $(C_OBJS)
